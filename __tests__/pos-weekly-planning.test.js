ddd12357db3f8e8e00922c0276bdb309a1ee7374
Dennis Lee <Dennis-LCL@users.noreply.github.com>
2019-07-30 00:59:19 +0800

    Add functionality to handle remainder when disaggregating weekly POS volume to dailly.

--- __tests__/pos-weekly-planning.test.js
+++ __tests__/pos-weekly-planning.test.js
@@ -4,13 +4,29 @@ describe("Sales volume forecast for a plan with 1 SKU and 1 WEEK using POS:", ()
   describe("Disaggregate weekly volume forecast to daily volume forecast using equal spread,", () => {
     describe("When total weekly volume forecast number is given,", () => {
       it("should return 7 equal daily volume when weekly volume is divisible by 7", () => {
-        dailyPosVolume = [20, 20, 20, 20, 20, 20, 20];
+        dailyPosVolume = [
+          { dayOfWeek: 1, posVolume: 20 },
+          { dayOfWeek: 2, posVolume: 20 },
+          { dayOfWeek: 3, posVolume: 20 },
+          { dayOfWeek: 4, posVolume: 20 },
+          { dayOfWeek: 5, posVolume: 20 },
+          { dayOfWeek: 6, posVolume: 20 },
+          { dayOfWeek: 7, posVolume: 20 }
+        ];
         expect(disaggregatePosVolume(140)).toMatchObject(dailyPosVolume);
       });
 
       it("should equally allocate the remainder (1) when weekly volume is NOT divisible by 7", () => {
         weeklyPosVolume = 8;
-        dailyPosVolume = [1, 1, 1, 1, 1, 1, 2];
+        dailyPosVolume = [
+          { dayOfWeek: 1, posVolume: 2 },
+          { dayOfWeek: 2, posVolume: 1 },
+          { dayOfWeek: 3, posVolume: 1 },
+          { dayOfWeek: 4, posVolume: 1 },
+          { dayOfWeek: 5, posVolume: 1 },
+          { dayOfWeek: 6, posVolume: 1 },
+          { dayOfWeek: 7, posVolume: 1 }
+        ];;
         expect(disaggregatePosVolume(weeklyPosVolume)).toMatchObject(
           dailyPosVolume
         );
@@ -18,12 +34,58 @@ describe("Sales volume forecast for a plan with 1 SKU and 1 WEEK using POS:", ()
 
       it("should equally allocate the remainder (2) when weekly volume is NOT divisible by 7", () => {
         weeklyPosVolume = 9;
-        dailyPosVolume = [1, 1, 1, 1, 1, 2, 2];
+        dailyPosVolume = dailyPosVolume = [
+          { dayOfWeek: 1, posVolume: 2 },
+          { dayOfWeek: 2, posVolume: 2 },
+          { dayOfWeek: 3, posVolume: 1 },
+          { dayOfWeek: 4, posVolume: 1 },
+          { dayOfWeek: 5, posVolume: 1 },
+          { dayOfWeek: 6, posVolume: 1 },
+          { dayOfWeek: 7, posVolume: 1 }
+        ];;
+        expect(disaggregatePosVolume(weeklyPosVolume)).toMatchObject(
+          dailyPosVolume
+        );
+      });
+
+      it("should equally allocate the remainder (4) when weekly volume is NOT divisible by 7", () => {
+        weeklyPosVolume = 11;
+        dailyPosVolume = dailyPosVolume = [
+          { dayOfWeek: 1, posVolume: 2 },
+          { dayOfWeek: 2, posVolume: 2 },
+          { dayOfWeek: 3, posVolume: 2 },
+          { dayOfWeek: 4, posVolume: 2 },
+          { dayOfWeek: 5, posVolume: 1 },
+          { dayOfWeek: 6, posVolume: 1 },
+          { dayOfWeek: 7, posVolume: 1 }
+        ];;
+        expect(disaggregatePosVolume(weeklyPosVolume)).toMatchObject(
+          dailyPosVolume
+        );
+      });
+
+      it("should equally allocate the remainder (6) when weekly volume is NOT divisible by 7", () => {
+        weeklyPosVolume = 13;
+        dailyPosVolume = dailyPosVolume = [
+          { dayOfWeek: 1, posVolume: 2 },
+          { dayOfWeek: 2, posVolume: 2 },
+          { dayOfWeek: 3, posVolume: 2 },
+          { dayOfWeek: 4, posVolume: 2 },
+          { dayOfWeek: 5, posVolume: 2 },
+          { dayOfWeek: 6, posVolume: 2 },
+          { dayOfWeek: 7, posVolume: 1 }
+        ];;
         expect(disaggregatePosVolume(weeklyPosVolume)).toMatchObject(
           dailyPosVolume
         );
       });
     });
+
+    describe("When % uplift on top of baseline volume is given,", () => {
+      it("should return 7 equal daily volume when weekly volume is divisible by 7", () => {
+        
+      })
+    })
   });
 });
 
--- src/pos-weekly-planning.js
+++ src/pos-weekly-planning.js
@@ -1,12 +1,28 @@
 const disaggregatePosVolume = weeklyVolume => {
-  const dailyPosVolume = [];
+  const dailyPosVolume = [
+    { dayOfWeek: 1, posVolume: 0 },
+    { dayOfWeek: 2, posVolume: 0 },
+    { dayOfWeek: 3, posVolume: 0 },
+    { dayOfWeek: 4, posVolume: 0 },
+    { dayOfWeek: 5, posVolume: 0 },
+    { dayOfWeek: 6, posVolume: 0 },
+    { dayOfWeek: 7, posVolume: 0 }
+  ];
   const volumeQuotient = Math.floor(weeklyVolume / 7);
-  const volumeRemainder = weeklyVolume % 7;
-  for (let i = 0; i < 6; i++) {
-    dailyPosVolume.push(volumeQuotient);
-  }
-  dailyPosVolume.push(volumeQuotient + volumeRemainder);
-  return dailyPosVolume;
+  let volumeRemainder = weeklyVolume % 7;
+
+  return dailyPosVolume.map(dayOfWeek => {
+    dayOfWeek.posVolume =
+      volumeRemainder > 0 ? volumeQuotient + 1 : volumeQuotient;
+    volumeRemainder -= 1;
+    return dayOfWeek;
+  });
+
+  // for (let i = 0; i < 6; i++) {
+  //   dailyPosVolume.push(volumeQuotient);
+  // }
+  // dailyPosVolume.push(volumeQuotient + volumeRemainder);
+  // return dailyPosVolume;
 };
 
 module.exports = disaggregatePosVolume;
